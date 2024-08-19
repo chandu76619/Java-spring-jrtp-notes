c2cde9c8a2baedaa31d2dcfb84d2a3a9f564bf5c
chandu00319 <you@example.com>
2024-08-19 13:11:59 +0530

    initial commit

---
+++ java/Demo.java
@@ -0,0 +1,17 @@
+/*java 100
+cpp 65
+python 50
+Sample Output
+
+================================
+java           100 
+cpp            065 
+python         050 
+================================*/
+import java.util.Scanner;
+class Demo{
+	public static void main (String [] args){
+		System.out.println("hello");
+			System.out.println("hello1");
+	}
+}
